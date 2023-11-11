96d676f3497797254d1799252202e3a5f4f40f16
Alexander Kopetsch <akopetsch92@gmail.com>
2023-11-11 14:41:38 +0100

    rename again to 'ExampleMe.NuGetXYZ' (ps1)

--- create-project.ps1
+++ create-project.ps1
@@ -1,16 +1,16 @@
 # Create a class library project and a test project
-dotnet new classlib -f netstandard2.1 -o SampleNuGetXYZ
-dotnet new xunit -f net6.0 -o SampleNuGetXYZ.Tests
+dotnet new classlib -f netstandard2.1 -o ExampleMe.NuGetXYZ
+dotnet new xunit -f net6.0 -o ExampleMe.NuGetXYZ.Tests
 
-dotnet add SampleNuGetXYZ.Tests reference SampleNuGetXYZ
+dotnet add ExampleMe.NuGetXYZ.Tests reference ExampleMe.NuGetXYZ
 
 # Add MinVer to handle package versioning based on the commit / tag
-dotnet add SampleNuGetXYZ package MinVer
+dotnet add ExampleMe.NuGetXYZ package MinVer
 
 # Create a sln containing both projects
-dotnet new sln --name SampleNuGetXYZ
-dotnet sln add SampleNuGetXYZ
-dotnet sln add SampleNuGetXYZ.Tests
+dotnet new sln --name ExampleMe.NuGetXYZ
+dotnet sln add ExampleMe.NuGetXYZ
+dotnet sln add ExampleMe.NuGetXYZ.Tests
 
 # Create a global.json file to specify the required .NET SDK version.
 # This file is used by GitHub Actions to install the required .NET SDK version.
